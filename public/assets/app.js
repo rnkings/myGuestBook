d02ccb6692363187e853720daee39731d80cea63
Rose Koenings <rnkings.05@gmail.com>
2016-11-08 22:47:17 -0600

    got form show isShown to work and data from that database to work as well

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,118 +1,91 @@
-"use strict";
+'use strict';
 
 // BELOW IS TO HIDE LOGGIN after filled out
 // AND SHOW THE NEXT form to fill out guest book
 
-var app = angular.module('guestBook');
+var app = angular.module('guestbook', []);
 app.run(
 	[
-		"$rootScope", 
-		function($rootScope){
-			$rootScrope.loggedIn = false;
+		'$rootScope',
+		function ($rootScope) {
+			$rootScope.loggedIn = false;
 		}
 	]
 );
 
-
-
-app.controller('SignInController', ['$rootscope', function ($rootScope)]{
-  this.login = function(){
-    $rootScope.loggedIn =true;
-  };
-});
+app.controller('SignInController', ['$rootScope', '$http', '$scope', function ($rootScope, $http, $scope) {
+	this.login = function () {
+		$http({
+			method: 'POST',
+			url: '/submit/signInForm',
+			data: {
+				firstname: $scope.firstname,
+				lastname: $scope.lastname,
+				weddingname: $scope.weddingname
+			}
+		}).then(function (response) {
+			console.log(response);
+			$rootScope.loggedIn = true;
+			$rootScope.user = response.data.user;
+			$rootScope.wedding = response.data.wedding;
+		}).catch(function (error) {
+			alert("You did not type in the correct information.  Please try again.");
+		});
+		// $rootScope.loggedIn = true;
+	};
+}]);
 
 // Below is creating tabs, if they are shown and fake hard coded message info
 
-(function () {
-	var app = angular.module('guestbook', []);
+app.controller('TabController', function () {
+	this.tab = 2;
 
-	app.controller('TabController', function () {
-		this.tab = 2;
+	this.showTab = function (tab) {
+		this.tab = tab;
+	};
 
-		this.showTab = function (tab) {
-			this.tab = tab;
-		};
+	this.isShown = function (tab) {
+		return this.tab === tab;
+	};
+});
 
-		this.isShown = function (tab) {
-			return this.tab === tab;
-		};
-	});
-
-	app.controller('MessageController', function () {
-		this.messages = [
-			{
-				message: 'You did it!',
-				address: '123 Fake St',
-				weddingInfo = [
-					{
-						date: 'January 5, 2018'
-						invited: 208
-						time: '7 a.m.'
-						location: 'Botanical Gardens'
-						letter: 'You ' + user.firstName + ' ' + user.lastName + 
-						' ' + 'are invited to' + ' ' + user.weddingName;
-						rvsp:
-					}
-				]
-			},
-			{
-				message: 'Congrats sweetie!',
-				address: '456 Fake St'
-				weddingInfo = [
-					{
-						date: 'January 5, 2018'
-						invited: 208
-						time: '7 a.m.'
-						location: 'Botanical Gardens'
-						letter: 'You ' + user.firstName + ' ' + user.lastName + 
-						' ' + 'are invited to' + ' ' + user.weddingName;
-						rvsp:
-					}
-				]
-
-			},
-			{
-				message: 'So glad to spend this special day with you!',
-				address: '789 Fake St'
-				weddingInfo = [
-					{
-						date: 'January 5, 2018'
-						invited: 208
-						time: '7 a.m.'
-						location: 'Botanical Gardens'
-						letter: 'You ' + user.firstName + ' ' + user.lastName + 
-						' ' + 'are invited to' + ' ' + user.weddingName;
-						rvsp:
-					}
-				]
-			},
-			{
-				message: 'You two are perfect for each other',
-				address: '999 Fake St'
-				weddingInfo = [
-					{
-						date: 'January 5, 2018'
-						invited: 208
-						time: '7 a.m.'
-						location: 'Botanical Gardens'
-						letter: 'You ' + user.firstName + ' ' + user.lastName + 
-						' ' + 'are invited to' + ' ' + user.weddingName;
-						rvsp:
-					}
-				]
+app.controller('EnteredInfoController', ['$http', '$scope', '$rootScope', function ($http, $scope, $rootScope) {
+	this.addMessage = function () {
+		var user = $rootScope.user;
+		var wedding = $rootScope.wedding;
+		$http({
+			method: 'POST',
+			url: '/submit/weddingGuestFormInfo',
+			data: {
+				address: $scope.address,
+				message: $scope.message,
+				firstname: user.firstname,
+				lastname: user.lastname,
+				weddingID: wedding.weddingID
 			}
-		];
+		}).then(function(data){
+			alert('Success!');
+			console.log(data);
+		}).catch(function(error){
+			console.error(error);
+		});
+	};
+}]);
 
-		this.newMessage = {};
+		// var newMessage = $scope.newMessage;
 
-		this.addMessage = function () {
-			this.messages.push(this.newMessage);
-			this.newMessage = {};
-		}
-	});
-})();
-
-
-
-
-
+		// $http.post({
+		// 		url: 'localhost:3001/submit/weddingGuestFormInfo',
+		// 		data: {
+		// 			address: $scope.address,
+		//
+		// 		}
+		// 	})
+		// 	.success(function(data){
+		// 		$scope.message = data;
+		// 	})
+		// 	.error(function(err){
+		// 		$log.error(err);
+		// 	});
+// 	}
+// }]);
